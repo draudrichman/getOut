9f3f4dea725a758e091f2825559c429a7d415da1
draudrichman <88120992+draudrichman@users.noreply.github.com>
2025-03-22 17:48:49 +0600

    added bookmarks

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,68 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import Loader from '@/components/Loader';
+import { COLORS } from '@/constants/theme';
+import { api } from '@/convex/_generated/api';
+import { styles } from '@/styles/feed.styles';
+import { useQuery } from 'convex/react';
+import { Image } from 'expo-image';
+import { View, Text, ScrollView } from 'react-native'
+
 
 export default function Saved() {
+  const savedPosts = useQuery(api.saved.getSavedPosts);
+
+  if (savedPosts === undefined) return <Loader />
+  if (savedPosts?.length === 0) return <NoSavedPostsFound />
+
   return (
-    <View>
-      <Text>Saved</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Saved</Text>
+      </View>
+
+      <ScrollView
+        contentContainerStyle={{
+          padding: 8,
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        {savedPosts.map((post) => {
+          if (!post) return null;
+          return (
+            <View key={post._id} style={{ width: "33.33%", padding: 1 }}>
+              <Image
+                source={post.imageUrl}
+                style={{ width: "100%", aspectRatio: 1 }}
+                contentFit="cover"
+                transition={200}
+                cachePolicy="memory-disk"
+              />
+            </View>
+          )
+        })}
+      </ScrollView>
+    </View>
+  )
+}
+
+function NoSavedPostsFound() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Saved</Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: COLORS.background,
+        }}
+      >
+        <Text style={{ fontSize: 22, color: COLORS.white }}>
+          No saved posts yet
+        </Text>
+      </View>
     </View>
   )
 }
